4810c06c8f323b401e23f73e7b6b8c0b5aedc0db
SrujanSShetty <SrujanSShettyofficial@gmail.com>
2025-02-18 06:06:25 -0600

    Fix error handling and print statements

--- Apache Spark/San Fransico Fire Analysis/San Fransico FireCalls Analysis.py
+++ Apache Spark/San Fransico Fire Analysis/San Fransico FireCalls Analysis.py
@@ -18,44 +18,61 @@ if __name__=="__main__":
     file_path = sys.argv[1]
     
     #Reading the file into Spark DataFrame using the inferred method having CSV format
-    df = (spark.read.csv("csv")
+    df = (spark.read
           .option("header","true")
           .option("inferSchema","true")
-          .load(file_path))
+          .csv(file_path))
     
     #Convert CallDate Column to Date format
-    df=df.withcolumn("CallDate", to_date(col("CallDate","MM/dd/yyyy")))
+    df= df.withColumn("CallDate", to_date(col("CallDate"),"MM/dd/yyyy"))
+    
+    # Drop rows with null CallDate
+    df = df.dropna(subset=["CallDate"])
     
     #Answering all 7 questions in order
     
     #1.What were all the different types of fire calls in 2018?
+    print("\n 1.What were all the different types of fire calls in 2018?:")
     fire_calltype_2018 = df.filter(year("CallDate")== 2018).select("CallType").distinct()
-    print(f"What were all the different types of fire calls in 2018?: {fire_calltype_2018.show()}")
+    fire_calltype_2018.show()
+    
     
     #2.What months within the year 2018 saw the highest number of fire calls?
+    print("\n 2.What months within the year 2018 saw the highest number of fire calls?:")
     highest_firecalls_by_monthof2018 = df.filter(year("CallDate")==2018).groupBy(month("CallDate").alias("Month")).count().orderBy(col("count").desc())
-    print(f"2.What months within the year 2018 saw the highest number of fire calls?:{highest_firecalls_by_monthof2018.show()}")
+    highest_firecalls_by_monthof2018.show()
     
+                     
     #3.Which neighborhood in San Francisco generated the most fire calls in 2018?
+    print("\n 3.Which neighborhood in San Francisco generated the most fire calls in 2018?:")
     highest_firecalls_by_neighborhood2018 = df.filter(year("CallDate")==2018).groupBy("Neighborhood").count().orderBy(col("count").desc())
-    print(f"3.Which neighborhood in San Francisco generated the most fire calls in 2018?:{highest_firecalls_by_neighborhood2018.show()}")
+    highest_firecalls_by_neighborhood2018.show()
+    
     
     #4.Which neighborhoods had the worst response times to fire calls in 2018?
+    print("\n 4.Which neighborhoods had the worst response times to fire calls in 2018?:")
     worst_responsetime_neighborhood2018 = df.filter(year("CallDate")==2018).groupBy("Neighborhood").agg(avg("Delay").alias("Avg_response_time")).orderBy(col("Avg_response_time").desc())
-    print(f"4.Which neighborhoods had the worst response times to fire calls in 2018?:{worst_responsetime_neighborhood2018.show()}")
+    worst_responsetime_neighborhood2018.show()
+    
     
     #5.Which week in the year in 2018 had the most fire calls?
+    print("\n 5.Which week in the year in 2018 had the most fire calls?:")
     weekintheyear2018_most_firecalls = df.filter(year("CallDate")==2018).groupBy(weekofyear("CallDate").alias("Week")).count().orderBy(col("count").desc())
-    print(f"5.Which week in the year in 2018 had the most fire calls?:{weekintheyear2018_most_firecalls.show()}")
+    weekintheyear2018_most_firecalls.show()
+    
     
     #6.Is there a correlation between neighborhood, zip code, and number of fire calls?
+    print("\n 6.Is there a correlation between neighborhood, zip code, and number of fire calls?:")
     correlation = df.filter(year("CallDate")==2018).groupBy("Neighborhood","Zipcode").count().orderBy(col("count").desc())
-    print(f"6.Is there a correlation between neighborhood, zip code, and number of fire calls?:{correlation.show(10)}")#Top 10 count in descending
+    correlation.show(10)#Top 10 count in descending
+    
     
     #7.How can we use Parquet files or SQL tables to store this data and read it back?
+    print("\n 7. Storing and reading data using Parquet:")
     df.write.format("parquet").mode("overwrite").save("sf-fire-calls.parquet")
     parquet_df=spark.read.parquet("sf-fire-calls.parquet")
     parquet_df.show(6)
     
     #Stop Spark Session
     spark.stop()
+    
