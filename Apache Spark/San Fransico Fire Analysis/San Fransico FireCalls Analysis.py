f6b54af7f3b940b5fc7d79f1d88409ae5bd14565
SrujanSShetty <SrujanSShettyofficial@gmail.com>
2025-02-18 03:21:53 -0600

    Reading the file into spark

---
+++ Apache Spark/San Fransico Fire Analysis/San Fransico FireCalls Analysis.py
@@ -0,0 +1,24 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count, year, month, weekofyear, avg, to_date
+
+if __name__=="__main__":
+    if len(sys.argv)!=2:
+        print("Usage: module-05:py <file>", file=sys.stderr)
+        sys.exit(-1)
+
+    #Building the Spark Session using the SparkSession APIs
+    spark = (SparkSession
+         .builder
+         .appname("SF Fire Calls Analysis")
+         .getOrCreate())
+    
+    #Get the SF fire calls dataset path from the Command line.
+    file_path = sys.argv[1]
+    
+    #Reading the file into Spark DataFrame using the inferred method having CSV format
+    df = (spark.read.csv("csv")
+          .option("header","true")
+          .option("inferSchema","true")
+          .load(file_path))
